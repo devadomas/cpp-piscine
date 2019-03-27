7b740b407003bb88b351b1967dc99ebca1945762
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-03-27 18:03:54 +0200

    [done] day00ex00

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/27 17:31:19 by azaliaus          #+#    #+#             */
+/*   Updated: 2019/03/27 18:02:17 by azaliaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int		main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (size_t j = 0; j < strlen(argv[i]); j++)
+				argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
+}
