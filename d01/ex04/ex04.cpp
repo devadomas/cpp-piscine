1b8791b14d4a0ab69aa7f3ee5b2a6fbf25d3f825
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-19 16:52:58 +0200

    d01ex04

---
+++ d01/ex04/ex04.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+int			main(void)
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string		*ptr = &str;
+	std::string		&ref = str;
+
+	std::cout << "Using pointer: " << *ptr << std::endl;
+	std::cout << "Using referance: " << ref << std::endl;
+}
