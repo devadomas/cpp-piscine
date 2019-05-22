9e49783944e0f4e64367d9f57608dc7b115b87d5
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-22 17:05:50 +0200

    as said no jokes d01ex04

--- d01/ex04/ex04.cpp
+++ d01/ex04/ex04.cpp
@@ -4,8 +4,8 @@
 int			main(void)
 {
 	std::string		str = "HI THIS IS BRAIN";
-	std::string		*ptr = &str;
-	std::string		&ref = str;
+	std::string	*	ptr = &str;
+	std::string	&	ref = str;
 
 	std::cout << "Using pointer: " << *ptr << std::endl;
 	std::cout << "Using referance: " << ref << std::endl;
