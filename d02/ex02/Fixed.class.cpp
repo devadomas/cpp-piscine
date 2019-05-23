03945d645e860b431148cc3919a42a7afb285580
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-23 15:35:41 +0200

    d02ex02 - deleted debugging messages

--- d02/ex02/Fixed.class.cpp
+++ d02/ex02/Fixed.class.cpp
@@ -4,35 +4,18 @@
 
 const int	Fixed::_bits = 8;
 
-Fixed::Fixed(void): _value(0)
-{
-	std::cout << "Default constructor called" << std::endl;
-}
-
-Fixed::Fixed(int const value): _value(value << Fixed::_bits)
-{
-	std::cout << "Int constructor called" << std::endl;
-}
-
-Fixed::Fixed(float const value): _value(roundf(value * (1 << Fixed::_bits)))
-{
-	std::cout << "Float constructor called" << std::endl;
-}
+Fixed::Fixed(void): _value(0) { }
+Fixed::Fixed(int const value): _value(value << Fixed::_bits) { }
+Fixed::Fixed(float const value): _value(roundf(value * (1 << Fixed::_bits))) { }
+Fixed::~Fixed(void) { }
 
 Fixed::Fixed(Fixed const &src)
 {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
-}
-
-Fixed::~Fixed(void)
-{
-	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed &		Fixed::operator=(Fixed const & src)
 {
-	std::cout << "Assignation operator called" << std::endl;
 	if (this != &src)
 		this->_value = src.getRawBits();
 	return *this;
@@ -40,7 +23,6 @@ Fixed &		Fixed::operator=(Fixed const & src)
 
 int			Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_value;
 }
 
