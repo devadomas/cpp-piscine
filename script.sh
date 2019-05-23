c9424fe2bedc99dcf86be3e1280f9f3e263fe817
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-23 20:38:24 +0200

    Damien made his script even shitter(better)

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 # Parameters
 if [ "$#" -ne 2 ]; then
-    echo "Please enter 2 parameters."
-    exit
+    echo "usage: ./compare.sh ../vogsphere-cpp/d01 days/day-01"
+	exit
 fi
 
 # Clear screen
@@ -15,20 +15,52 @@ for directory in $1/*/ ; do
 
     printf "\e[1m\e[94m$directory\e[0m\e[21m\n\n"
 
-    # Loop through directories given in second parameter
-    for file in $directory*; do
+	# Loop through directories given in second parameter
+	for file in $directory*; do
 
-        vogsphereFile="${file/$1/$2}"
-        if [ ! -f $vogsphereFile ]; then
-            printf "\e[91m|-- ${vogsphereFile##*/} : file not found\e[0m\n"
-        elif diff $file $vogsphereFile >/dev/null; then
-            printf "\e[92m|-- ${vogsphereFile##*/} : ok\e[0m\n"
-        else
-            printf "\e[91m|-- ${vogsphereFile##*/} : difference found\e[0m\n"
-        fi
+		vogsphereFile="${file/$1/$2}"
 
-    done
+		# Check if file exists
+		if [ ! -f $vogsphereFile ]; then
+			printf "\e[91m|-- ${vogsphereFile##*/} : file not found\e[0m\n"
 
-    echo ""
+		# Make a diff on files
+		elif diff $file $vogsphereFile >/dev/null; then
+			printf "\e[92m|-- ${vogsphereFile##*/} : ok\e[0m\n"
+		else
+			printf "\e[91m|-- ${vogsphereFile##*/} : difference found\e[0m\n"			
+		fi
+
+	done
+
+	echo ""
 
 done
+
+# Check git status
+CURPWD=$(printenv PWD)
+
+printf "\e[1m\e[94mGIT status\e[0m\e[21m\n\n"
+cd $1
+if git status | grep -q "Changes not staged for commit:"; then
+	printf "\e[91m|-- $1 : Changes not staged for commit\e[0m\n"
+elif git status | grep -q "Changes to be committed:"; then
+	printf "\e[91m|-- $1 : Changes to be committed\e[0m\n"
+elif git status | grep -q "to publish your local commits"; then
+	printf "\e[91m|-- $1 : Changes to be published\e[0m\n"
+else
+	printf "\e[92m|-- $1 : Branch is up-to-date\e[0m\n"
+fi
+cd $CURPWD
+cd $2
+if git status | grep -q "Changes not staged for commit:"; then
+	printf "\e[91m|-- $2 : Changes not staged for commit\e[0m\n"
+elif git status | grep -q "Changes to be committed:"; then
+	printf "\e[91m|-- $2 : Changes to be committed\e[0m\n"
+elif git status | grep -q "to publish your local commits"; then
+	printf "\e[91m|-- $2 : Changes to be published\e[0m\n"
+else
+	printf "\e[92m|-- $2 : Branch is up-to-date\e[0m\n"
+fi
+cd $CURPWD
+
