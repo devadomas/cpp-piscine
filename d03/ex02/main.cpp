40f21f6b94887cb56b7e24ed8cbd0df53f6b34e3
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-24 14:00:56 +0200

    d03ex01 - Makefile&main.cpp changed from previous ex

--- d03/ex02/main.cpp
+++ d03/ex02/main.cpp
@@ -4,12 +4,37 @@
 
 int		main(void)
 {
-	FragTrap	claptrap("Keren");
-	ScavTrap	robot("K1ller");
+	FragTrap			*trap = new FragTrap("K!LL3R");
 
-	claptrap.vaulthunter_dot_exe("John");
-	robot.takeDamage(10); // should take 3 less
+	trap->rangedAttack("Zer0");
+	//
+	trap->meleeAttack("Betty");
+	trap->rangedAttack("Nelly");
+	std::cout << *trap;
+	// next!
+	trap->takeDamage(200);
+	std::cout << *trap;
+	trap->beRepaired(500);
+	trap->takeDamage(1);
+	std::cout << *trap;
+	trap->vaulthunter_dot_exe("Terminator");
+	trap->vaulthunter_dot_exe("Norminattor");
+	trap->vaulthunter_dot_exe("Verificator");
+	trap->vaulthunter_dot_exe("Terminator");
+	trap->vaulthunter_dot_exe("Norminattor");
+	trap->vaulthunter_dot_exe("Verificator");
+	std::cout << *trap;
+	delete trap;
+	//
+	std::cout << "----- Test's for ScavTrap ------" << std::endl;
 
-	std::cout << robot;
-	// destructor..
+	ScavTrap			trappy("Handsome Jack");
+
+	trappy.rangedAttack("K!LL3R");
+	trappy.rangedAttack("Something");
+	// more
+	std::cout << trappy;
+	std::cout << "Doing takeDamage for 10HP, should take 3HP LESS" << std::endl;
+	trappy.takeDamage(10); // should take 3 less.
+	std::cout << trappy;
 }
