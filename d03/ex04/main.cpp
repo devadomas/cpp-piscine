230094670fbd14995398929e14e52e4f68e6b9e3
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-24 15:50:53 +0200

    d02ex04 - better test's of main

--- d03/ex04/main.cpp
+++ d03/ex04/main.cpp
@@ -4,34 +4,23 @@
 int		main(void)
 {
 	std::cout << "------------------ Beggining of main  ------------------" << std::endl;
-
-	FragTrap	claptrap("Keren");
-
-	claptrap.rangedAttack("John");
-	claptrap.meleeAttack("John");
-	claptrap.takeDamage(10);
-	claptrap.beRepaired(25);
-	claptrap.meleeAttack("S0M!T");
-	SuperTrap	newTrap("Wick");
-	std::cout << "Proofing that assignation worked: " << newTrap.getName() << std::endl;
-	std::cout << claptrap;
-
-	std::cout << "------------------------" << std::endl;
-
-	SuperTrap		*someTrap = new SuperTrap("R0B0T");
-	someTrap->vaulthunter_dot_exe("KYBORG");
-	someTrap->ninjaShoebox(claptrap);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	someTrap->takeDamage(20);
-	std::cout << "Test of damage repair to full" << std::endl;
-	someTrap->beRepaired(500);
-	std::cout << *someTrap;
-
-	std::cout << "---------------- END OF TESTS MAIN ---------------- " << std::endl;
+	SuperTrap *			trap = new SuperTrap("SuperTrap");
+	NinjaTrap			ninja("NinjaTrap");
+	FragTrap			frag("FragTrap");
+	std::cout << "------------------" << std::endl;
+	std::cout << *trap;
+	std::cout << "------------------" << std::endl;
+	std::cout << ninja;
+	std::cout << "------------------" << std::endl;
+	std::cout << frag;
+	// attacks
+	std::cout << "------------------" << std::endl;
+	trap->meleeAttack("Something");
+	trap->rangedAttack("Someone");
+	std::cout << "------------------" << std::endl;
+	// random damages ant etc
+	trap->takeDamage(50);
+	std::cout << *trap;
+	std::cout << "------------------ Beggining of main  ------------------" << std::endl;
+	delete trap; // as always: no loose ends
 }
