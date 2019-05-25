dde7b741d16a59dcb8170c0c07faa0539e8a7055
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-25 22:49:53 +0200

    main improved

--- d04/ex03/main.cpp
+++ d04/ex03/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "AMateria.hpp"
 #include "Character.hpp"
 #include "Cure.hpp"
@@ -7,24 +8,59 @@
 
 int		main(void)
 {
-		IMateriaSource* src = new MateriaSource();
-		src->learnMateria(new Ice());
-		src->learnMateria(new Cure());
+	std::cout << "--- Tests from subject ---" << std::endl;
 
-		ICharacter* zaz = new Character("zaz");
+	Ice*			ice = new Ice;
+	Cure*			cure = new Cure;
+	IMateriaSource* src = new MateriaSource();
 
-		AMateria* tmp;
-		tmp = src->createMateria("ice");
-		zaz->equip(tmp);
-		tmp = src->createMateria("cure");
-		zaz->equip(tmp);
+	src->learnMateria(ice);
+	src->learnMateria(cure);
 
-		ICharacter* bob = new Character("bob");
+	Character* zaz = new Character("zaz");
 
-		zaz->use(0, *bob);
-		zaz->use(1, *bob);
-		delete bob;
-		delete zaz;
-		delete src;
-		return 0;
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	zaz->equip(tmp);
+	tmp = src->createMateria("cure");
+	zaz->equip(tmp);
+
+	Character* bob = new Character("bob");
+
+	zaz->use(0, *bob);
+	zaz->use(1, *bob);
+
+	std::cout << std::endl;
+	///
+	std::cout << "---- More tests ----" << std::endl;
+	std::cout << "Try to use wrong weapon index" << std::endl;
+	zaz->use(3, *bob);
+	std::cout << "Try to equip equiped AMateria" << std::endl;
+	zaz->equip(tmp);
+	zaz->use(2, *bob);
+	std::cout << "Try to unequip non-existing AMateria" << std::endl;
+	zaz->unequip(2);
+	std::cout << "Try to learn materia already in sources" << std::endl;
+	src->learnMateria(ice);
+
+	// copy
+	Character	zazCopy(*zaz);
+	zazCopy.equip(src->createMateria("ice"));
+	zazCopy.use(1, *bob);
+	zazCopy.use(1, *bob);
+	zazCopy.use(1, *bob);
+
+	// assiganation
+	Character	zazAssign = zazCopy;
+	zazAssign.use(0, *bob);
+	zazAssign.use(0, *bob);
+	zazAssign.use(1, *bob);
+	std::cout << std::endl;
+
+	delete bob;
+	delete zaz;
+	delete src;
+	delete ice;
+	delete cure;
+	return 0;
 }
