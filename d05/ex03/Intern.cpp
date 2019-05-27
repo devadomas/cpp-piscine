6e68924d46773cef5e79bc7615f03bbad03b78ce
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-27 19:18:38 +0200

    d05ex03 - array typo in Intern class

--- d05/ex03/Intern.cpp
+++ d05/ex03/Intern.cpp
@@ -19,7 +19,7 @@ Form *			Intern::makeForm(std::string const & request, std::string const target)
 {
 	std::string		arr[] = {
 		"robotomy request",
-		"presidential pardon"
+		"presidential pardon",
 		"shrubbery creation"
 	};
 	typedef Form* (Intern::*FormCreation)(std::string const) const;
@@ -31,17 +31,19 @@ Form *			Intern::makeForm(std::string const & request, std::string const target)
 
 
 	for (int i = 0; i < 3; i++)
+	{
 		if (request == arr[i])
 		{
 			std::cout << "Intern is about to create " << request << " form" << std::endl;
 			return (this->*(forms[i]))(target);
 		}
-	throw Intern::InternNoFormFound();
+	}
+	throw Intern::InternNoFormFoundException();
 	return NULL;
 }
 
 Intern::InternNoFormFoundException::InternNoFormFoundException(void) { }
 Intern::InternNoFormFoundException::~InternNoFormFoundException(void) throw() { }
-Intern::InternNoForInternNoFormFoundExceptionmFound::InternNoFormFoundException(InternNoFormFoundException const &src) { *this = src; }
+Intern::InternNoFormFoundException::InternNoFormFoundException(InternNoFormFoundException const & src) { *this = src; }
 Intern::InternNoFormFoundException & Intern::InternNoFormFoundException::operator=(Intern::InternNoFormFoundException const &) { return *this; }
 const char * Intern::InternNoFormFoundException::what(void) const throw() { return ("no matching form found"); }
