c8fcde8513c3d5233040efe94af31e053c34af12
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-28 21:08:56 +0200

    d07ex00 - main changes

--- d07/ex00/whatever.cpp
+++ d07/ex00/whatever.cpp
@@ -32,26 +32,21 @@ T			max(T const & s1, T const & s2)
 
 int		main(void)
 {
-	int		a = 5;
-	int		b = 6;
-	std::cout << "Before: " << std::endl
-				<< "a = " << a << std::endl
-				<< "b = " << b << std::endl;
-	::swap(a, b);
-	std::cout << "After: " << std::endl
-				<< "a = " << a << std::endl
-				<< "b = " << b << std::endl;
-	std::cout << "Min max test" << std::endl;
-	std::cout << "min(a, b) == " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) == " << ::max(a, b) << std::endl;
+	int		a = 2;
+	int		b = 3;
 
-	std::cout << "It also works with strings!" << std::endl;
-	std::string			s1 = "Test", s2 = "Not testing";
-	std::cout << "Before: " << std::endl
-				<< "s1 = " << s1 << std::endl
-				<< "s2 = " << s2 << std::endl;
-	::swap(s1, s2);
-	std::cout << "After: " << std::endl
-				<< "s1 = " << s1 << std::endl
-				<< "s2 = " << s2 << std::endl;
+	::swap( a, b );
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
