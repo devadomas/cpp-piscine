8e0d98c22786edacb2a52ffecd6e2945a940f52b
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-28 20:50:01 +0200

    Implamented new main

--- d05/ex04/main.cpp
+++ d05/ex04/main.cpp
@@ -8,27 +8,38 @@
 
 int			main(void)
 {
+	std::srand(std::time(NULL));
 	std::cout << "--- Creatimg empty office ---" << std::endl;
 	Intern						intern;
 	Bureaucrat					signer("Signer", 1);
 	Bureaucrat					executor("Executor", 1);
 	OfficeBlock					office;
+	std::string					arr[] = {
+		"robotomy request",
+		"presidential pardon",
+		"shrubbery creation",
+		"useless garbage"
+	};
 
 	std::cout << "--- Now we will try to set people to work! ---" << std::endl;
 	office.setIntern(&intern);
 	office.setSigner(&signer);
 	office.setExecutor(&executor);
-	try
+	for (int i = 0; i < 5; i++)
 	{
-		office.doBureaucracy("robotomy reques1t", "Some guy");
+		try
+		{
+			office.doBureaucracy(arr[rand() % 4], "Some guy");
+		}
+		catch (OfficeBlock::OfficeNotFilledException & e)
+		{
+			std::cout << "Error occured: " << e.what() << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			/* oh god, unknown error, what to do ?! */
+			std::cout << "Exception cought: " << e.what() << std::endl;
+		}
 	}
-	catch (OfficeBlock::OfficeNotFilledException & e)
-	{
-		std::cout << "Error occured: " << e.what() << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		/* oh god, unknown error, what to do ?! */
-		std::cout << "Exception cought: " << e.what() << std::endl;
-	}
+
 }
