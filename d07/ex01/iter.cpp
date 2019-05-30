2a527d82d079e616c117bf0c2bc87a8880d66a0c
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-30 10:56:24 +0200

    d07ex01 - added different type of tests to show how tempaltes work

--- d07/ex01/iter.cpp
+++ d07/ex01/iter.cpp
@@ -1,5 +1,5 @@
+#include <string>
 #include <iostream>
-
 
 template< typename T >
 void		iter(T * arr, size_t length, void(&f)(T &))
@@ -20,11 +20,15 @@ void		print_arr(T & data)
 
 int			main(void)
 {
-	int		test[] =
-	{
+	int		test[] = {
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+	};
+	std::string	strArr[] = {
+		"Test1", "Test2", "Test3"
 	};
 
 	std::cout << "Testing: " << std::endl;
 	iter(test, 10, print_arr);
+	std::cout << "--- Will test with different type: string ---" << std::endl;
+	iter(strArr, 3, print_arr);
 }
