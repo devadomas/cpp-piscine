68b41638426a496fc830e5931a260d357028d4da
Adomas Žaliauskas <adomas.zal@gmail.com>
2019-05-30 15:11:30 +0200

    makeover of header file

--- d08/ex04/Solver.class.hpp
+++ d08/ex04/Solver.class.hpp
@@ -11,10 +11,10 @@ class Solver
 {
 public:
 
-	Solver(std::string const & input);
-	Solver(Solver const & src);
+	Solver(std::string const &);
+	Solver(Solver const &);
 	~Solver(void);
-	Solver&	operator=(Solver const & src);
+	Solver&	operator=(Solver const &);
 
 	void			tokenize(void);
 	bool			verify(void);
@@ -28,24 +28,23 @@ public:
 	void			printRPN(void) const;
 	std::string		addStack(std::vector<int>) const;
 
-	std::string		getInput(void) const;
+	std::string					getInput(void) const;
 	std::vector<std::string>	getTokens(void) const;
 	std::vector<std::string>	getStack(void) const;
 	std::vector<std::string>	getRPN(void) const;
 
 private:
-	std::string		_input;
-
-	std::vector<std::string>		_rpnV;
+	std::string						_input;
 	std::vector<int> 				_stack;
+	std::vector<std::string>		_rpnV;
 	std::vector<std::string>		_stackV;
 	std::vector<std::string>		_tokensV;
 	std::vector<std::string>		_commander;
 
-	static bool		isNumber(const std::string & str);
-	static float	toFloat(const std::string & str);
-	static std::string	formatToken(const std::string & str);
-	static std::string	applyPadding(const std::string & str);
+	static bool						isNumber(const std::string &);
+	static float					toFloat(const std::string &);
+	static std::string				formatToken(const std::string &);
+	static std::string				applyPadding(const std::string &);
 
 protected:
 	Solver(void);
